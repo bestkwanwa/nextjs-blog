f964635c62a8dc72cc924f9bb09f3caa84ceaae7
bestkwanwa <bestkwanwa@gmail.com>
2021-09-03 18:41:21 +0800

    fix

--- pages/index-unused.js
+++ pages/index-unused.js
@@ -1,31 +1,31 @@
-import Link from 'next/link'
-import Date from '../components/date'
-import utilStyles from '../styles/utils.module.css'
+// import Link from 'next/link'
+// import Date from '../components/date'
+// import utilStyles from '../styles/utils.module.css'
 
-// 测试getStaticProps在每次请求都执行
-// let count=0
-export default function Home(props) {
-  return (
-    <li className={utilStyles.listItem} key={id}>
-      <Link href={`/posts/${id}`}>
-        <a>{title}</a>
-      </Link>
-      <br />
-      <small className={utilStyles.lightText}>
-        <Date dateString={date} />
-      </small>
-    </li>
-  )
-}
+// // 测试getStaticProps在每次请求都执行
+// // let count=0
+// export default function Home(props) {
+//   return (
+//     <li className={utilStyles.listItem} key={id}>
+//       <Link href={`/posts/${id}`}>
+//         <a>{title}</a>
+//       </Link>
+//       <br />
+//       <small className={utilStyles.lightText}>
+//         <Date dateString={date} />
+//       </small>
+//     </li>
+//   )
+// }
 
-export async function getStaticProps() {
-  // Get external data from the file system, API, DB, etc.
-  const data = 'This is data.'
-  // let count = 0
+// export async function getStaticProps() {
+//   // Get external data from the file system, API, DB, etc.
+//   const data = 'This is data.'
+//   // let count = 0
 
-  // The value of the `props` key will be
-  //  passed to the `Home` component
-  return {
-    props: { data }
-  }
-}
+//   // The value of the `props` key will be
+//   //  passed to the `Home` component
+//   return {
+//     props: { data }
+//   }
+// }
